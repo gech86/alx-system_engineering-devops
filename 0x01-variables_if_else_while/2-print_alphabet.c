cd942fa654f93a765069714e6304c9a18a557dc8
Getachew Gebru <getachew123gebru@gmail.com>
2023-09-10 13:33:51 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,17 +8,9 @@ int main(void)
 
     char alphabet = 'a';
 
-     /* check to see if the current value of your variable is less than 
-         or the same as the last member (z) of the given range. While 
-         the value is less, go ahead and print the value 
-      */
-
     while (alphabet <= 'z')
-    {
-        // print the value of the variable 
+    { 
         putchar(alphabet);
-
-        // increment the variable (ie: a becomes b)
         alphabet++;
     }
 
